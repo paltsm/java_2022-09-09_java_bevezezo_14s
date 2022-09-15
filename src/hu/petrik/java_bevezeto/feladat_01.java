59668063b293dc7270dbbb030e0c516899ac015a
misi <misipalotas@gmail.com>
2022-09-15 17:01:07 +0200

    initial commit

---
+++ src/hu/petrik/java_bevezeto/feladat_01.java
@@ -0,0 +1,17 @@
+package hu.petrik.java_bevezeto;
+
+import java.util.Scanner;
+
+public class feladat_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("kérem adja meg a medence átmérőjét(m): ");
+        double atemero = sc.nextDouble();
+        System.out.print("kérem adja meg a medence mélységét(m): ");
+        double melyseg = sc.nextDouble();
+        double sugar = atemero / 2;
+        double terulet = sugar * sugar * Math.PI;
+        double terfogat = terulet * melyseg;
+        System.out.printf("a medencébe %.2f köbméter víz fér.",terfogat);
+    }
+}
---
+++ src/hu/petrik/java_bevezeto/feladat_09.java
@@ -0,0 +1,11 @@
+package hu.petrik.java_bevezeto;
+
+public class feladat_09 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 1000; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
