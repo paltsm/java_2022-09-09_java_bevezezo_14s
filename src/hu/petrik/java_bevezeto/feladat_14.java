ddbbc748000596e66099ca01c7d932815722f058
misi <misipalotas@gmail.com>
2022-09-15 17:34:43 +0200

    14. feledat kezdet

---
+++ src/hu/petrik/java_bevezeto/feladat_14.java
@@ -0,0 +1,11 @@
+package hu.petrik.java_bevezeto;
+
+import java.util.Scanner;
+
+public class feladat_14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("kérem adjon meg egy szövegete: ");
+        String szoveg = sc.nextLine();
+    }
+}
